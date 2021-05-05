87ba47d34c8028e186fb761c759e2984fe8df2aa
DESKTOP-M74EIJ9\johan <jthoms19@student.aau.dk>
2021-05-05 10:50:20 +0200

    Assign kan også tage imod bools nu igennem typecheck

--- src/ASTTypeCheck.java
+++ src/ASTTypeCheck.java
@@ -21,6 +21,8 @@ public class ASTTypeCheck extends ASTVisitor<String>{
                 return temp.Type;
             } else if (valueType.equals("ctrlStruc") && temp.Type.equals("number")) {
                 return "ctrlStruc";
+            } else if (valueType.equals("bool") && temp.Type.equals("number")) {
+                return "bool";
             } else if (!(valueType.equals("error"))) {
                 Errors.add("Assignment error: " + node.Target.value + " and " + valueType + " do not match");
             } else {
